8957a7608bf4618c70d64b16ac192371845a9b0c
vinney cavallo <vcavallo@gmail.com>
2022-09-20 20:53:26 -0400

    Add separate sticky nav

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <v-app class="tw-font-silom">
-    <div class="tw-container tw-mx-auto">
-      <header class="tw-flex tw-justify-around tw-mb-4">
-        gora
-      </header>
+    <header class="tw-flex tw-flex-col tw-fixed tw-bg-white tw-w-full tw-shadow-md tw-px-2" style="z-index: 50; height: 100px;">
+      <div class="tw-flex tw-justify-around tw-mb-4">
+        <h1 class="tw-text-3xl tw-text-secondary">
+          <router-link :to="{ name: 'pita' }">
+            GORA
+          </router-link>
+        </h1>
+      </div>
+      <nav class="tw-mb-4 tw-mx-1 md:tw-mx-0">
+        <Nav />
+      </nav>
+    </header>
 
+    <div class="tw-container tw-mx-auto" style="position: relative; top: 100px;">
       <div v-if="!haveSubscription || !havePita || !haveMeta || !haveLogs" class="tw-w-full tw-h-full tw-flex tw-flex-col">
         <div class="tw-my-4">
           <v-progress-linear height="25" color="info" rounded v-if="!haveSubscription" indeterminate>
@@ -30,20 +39,6 @@
       </div>
       <div v-else>
 
-        <div class="tw-mb-4">
-          <ul class="tw-flex tw-justify-between">
-            <li>
-            <router-link :to="{ name: 'pita' }">Goraverse</router-link>
-            </li>
-            <li>
-            <router-link :to="{ name: 'hedl' }">Hedl</router-link>
-            </li>
-            <li>
-            <router-link :to="{ name: 'mine' }">Mine</router-link>
-            </li>
-          </ul>
-        </div>
-
         <div>
           <router-view />
         </div>
@@ -56,6 +51,8 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { mapState, mapGetters } from 'vuex';
+
+import Nav from "@/components/nav.vue";
 
 export default defineComponent({
   mounted() {
@@ -81,6 +78,10 @@ export default defineComponent({
     closeAirlocks() {
       this.$store.dispatch("ship/closeAgentAirlocks");
     },
+  },
+
+  components: {
+    Nav
   },
 })
 </script>
---
+++ interface/src/components/nav.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="tw-flex tw-justify-between">
+    <li v-for="(dest, i) in pageLinks" :key="dest.route" class="tw-flex-1" :class="edges(i)">
+      <v-btn :variant="isActive(dest.route) ? 'tonal' : 'outlined'" color="surface" :to="{ name: dest.route }">
+        {{ dest.name }}
+      </v-btn>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      pageLinks: [
+        {
+          name: 'Goriverse',
+          route: 'pita',
+        },
+        {
+          name: 'Hedl',
+          route: 'hedl',
+        },
+        {
+          name: 'Mine',
+          route: 'mine',
+        },
+      ]
+    }
+  },
+
+  methods: {
+    isActive(routeName) {
+      return (this.$route.name === routeName)
+    },
+
+    edges(n) {
+      if (n === 0) {
+        return 'tw-text-left'
+      }
+      if (n === (this.pageLinks.length - 1)) {
+        return 'tw-text-right'
+      }
+      return 'tw-text-center'
+    },
+  },
+
+})
+</script>
+
